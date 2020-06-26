63b91a9f3696767917f241d527cabe9a19a25bc7
adamGiusti <adamgiusti@Adams-MacBook-Pro.local>
2020-06-26 11:08:01 -0700

    Add Robert's introduction

---
+++ src/Robert.java
@@ -0,0 +1,9 @@
+public class Robert {
+    void introduce() {
+        System.out.println("Hello there! I'm Robert, a bot developed using the Java programming language.");
+        System.out.println();
+
+        System.out.println("Care to chat? I'd love to talk with you about any of the following topics.");
+        System.out.println("Just select one of the options below (1, 2, 3):");
+    }
+}
